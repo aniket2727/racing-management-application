8f7c3f3a2b4bdf6350ba0f37e19660aff937f802
aniket2727 <aniketkadam5950@gmail.com>
2024-02-12 10:55:55 +0530

    Display post componets is created

---
+++ client/src/componets/Communitypost/DisplayPostComponets.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const DisplayPostComponents = () => {
+  return (
+    <div className="max-w-4xl mx-auto bg-white p-6 rounded-md shadow-md mt-8 min-h-300 border border-black">
+      <h1 className="text-2xl font-bold mb-4">Community Post</h1>
+
+      <div className="mb-4 border border-black rounded p-2">
+        <div className="flex items-center">
+          <h1 className="text-xl font-semibold mr-4">Aniket Kadam</h1>
+          <button className="bg-blue-500 text-white px-4 py-2 rounded">Follow</button>
+        </div>
+
+        {/* Post content goes here */}
+        <p className="text-gray-700">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+        </p>
+      </div>
+
+
+      <div className="mb-4 border border-black rounded p-2">
+      <div className="flex items-center">
+        <h1 className="text-xl font-semibold mr-4">Aniket Kadam</h1>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded">Follow</button>
+      </div>
+
+      {/* Post content goes here */}
+      <p className="text-gray-700">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+      </p>
+    </div>
+    </div>
+  );
+};
+
+export default DisplayPostComponents;
--- client/src/componets/Homecomponets/HomeComponets.js
+++ client/src/componets/Homecomponets/HomeComponets.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import SearchBarComponents from '../searchbar/SearchBarComponets'
 import CreateEventComponents from '../craeteevent/CreateeventComponets'
-
+import DisplayPostComponents from '../Communitypost/DisplayPostComponets'
 const HomeComponets = () => {
   return (
     <div>
     <SearchBarComponents/>
     <CreateEventComponents/>
+    <DisplayPostComponents/>
       
     </div>
   )
